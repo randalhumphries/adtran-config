b8191baf8b83b3c4a49e31ef3d76a77a6cd997ce
Randal Humphries <randal@randalhumphries.com>
2019-03-24 10:19:20 -0400

    Updates 'config' spec

--- spec/adtran/config_spec.rb
+++ spec/adtran/config_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Adtran::Config do
   it "has a version number" do
     expect(Adtran::Config::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
