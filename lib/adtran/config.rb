8b89903511e3913faaf97007d7a4fd5d376100f4
Randal Humphries <randal@randalhumphries.com>
2019-03-24 10:15:06 -0400

    Creates 'adtran-config' gem

---
+++ lib/adtran/config.rb
@@ -0,0 +1,8 @@
+require "adtran/config/version"
+
+module Adtran
+  module Config
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/adtran/config/version.rb
@@ -0,0 +1,5 @@
+module Adtran
+  module Config
+    VERSION = "0.1.0"
+  end
+end
